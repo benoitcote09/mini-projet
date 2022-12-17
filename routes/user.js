c5375390d1ea9e2cae9e4a1e6edcdf4a6ef34d42
Abbes <abbes.boulebtina@gmail.com>
2022-12-16 19:58:34 -0500

    project-vercel

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userRouter = express.Router();
 const passport = require('passport');
-const passportConfig = require('../passport');
+require('../passport');
 const User = require('../models/User');
 const Todo = require('../models/Todo');
 const JWT = require('jsonwebtoken');
